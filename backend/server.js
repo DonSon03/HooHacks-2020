3125d370f3483942d8bcc45c792d97c13137783a
Edwin Yu <theedwinyu888@gmail.com>
2020-03-28 13:22:59 -0400

    added twilio verify

--- backend/server.js
+++ backend/server.js
@@ -20,15 +20,68 @@ app.listen(port, () => {
 });
 
 app.post("/api/send_sms", function(req, res) {
-    var messageBody = req.body.message;
-    var sendToPhoneNumber = req.body.phone_number
+    const messageBody = req.body.message;
+    const sendToPhoneNumber = req.body.phone_number;
 
     client.messages.create({
         body: messageBody,
         from: twilioPhoneNumber,
         to: sendToPhoneNumber
     })
-    .then(message => console.log(message.sid));
+    .then(message => 
+        {
+            console.log(message.sid);
+            res.send('sent message (' + messageBody + ') to ' + sendToPhoneNumber);
+        }  
+    );
 
-    res.send('sent message (' + messageBody + ') to ' + sendToPhoneNumber);
+});
+
+app.get("/api/get_verification_service", function(req, res) {
+
+    const friendlyName = req.param('friendly_name');
+
+    client.verify.services.create({friendlyName: friendlyName})
+    .then(service =>
+        {
+            console.log(service.sid)
+            res.send({"data":service})
+        } 
+        
+    );
+
+});
+
+app.get("/api/send_verification_token", function(req, res) {
+    const sid = req.param('sid');
+    const phoneNumber = req.param('phone_number');
+
+    client.verify.services(sid)
+    .verifications
+    .create({to: phoneNumber, channel: 'sms'})
+    .then(verification => 
+        {
+            console.log(verification.status)
+            res.send({"data":verification})
+        }
+        
+    );
+
+});
+
+app.get("/api/check_verification_token", function(req, res) {
+
+    const sid = req.param('sid');
+    const phoneNumber = req.param('phone_number');
+    const code = req.param('code');
+
+    client.verify.services(sid)
+    .verificationChecks
+    .create({to: phoneNumber, code: code})
+    .then(verification_check => 
+        {
+            console.log(verification_check.status)
+            res.send({"data":verification_check})
+        }
+    );
 });
