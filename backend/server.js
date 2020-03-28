f803f9aab8c884485eb4bf4a5587ecb31464624d
Edwin Yu <theedwinyu888@gmail.com>
2020-03-28 11:59:16 -0400

    added gitignore and endpoint for send sms using twilio

--- backend/server.js
+++ backend/server.js
@@ -6,9 +6,28 @@ require('dotenv').config();
 const app = express();
 const port = process.env.PORT || 5000;
 
+const accountSid = process.env.TWILIO_ACCOUNT_SID;
+const authToken = process.env.TWILIO_AUTH_TOKEN;
+const twilioPhoneNumber = process.env.TWILIO_PHONE_NUMBER;
+
+const client = require('twilio')(accountSid, authToken);
+
 app.use(cors());
 app.use(express.json());
 
 app.listen(port, () => {
-    console.log('Server is running on port: $(port)');
+    console.log('Server is running on port: ' + port);
+});
+
+app.post("/api/send_sms", function(req, res) {
+    var messageBody = req.body.message;
+
+    client.messages.create({
+        body: messageBody,
+        from: twilioPhoneNumber,
+        to: '+12403867154'
+    })
+    .then(message => console.log(message.sid));
+
+    res.send('sent message: ' + messageBody);
 });
